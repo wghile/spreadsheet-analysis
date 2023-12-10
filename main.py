90e9d0dbf428811104f2ed1dbe04af636cf836ac
Wentana Ghile <137902774+wghile@users.noreply.github.com>
2023-12-09 23:30:42 -0500

    calculated total sales

--- main.py
+++ main.py
@@ -16,5 +16,20 @@ with open('Project/sales.csv', 'r') as csv_file:
 
 # 3. Output the total sales across all months
 months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
-print(months)
-# with open('Project/sales.csv', 'r')
+months_dict = [{'month': 'jan', 'total_sales': 0}, {'month': 'feb', 'total_sales': 0}, {'month': 'mar', 'total_sales': 0}, {'month': 'apr', 'total_sales': 0}, {'month': 'may', 'total_sales': 0}, {'month': 'jun', 'total_sales': 0}, {'month': 'jul', 'total_sales': 0}, {'month': 'aug', 'total_sales': 0}, {'month': 'sep', 'total_sales': 0}, {'month': 'oct', 'total_sales': 0}, {'month': 'nov', 'total_sales': 0}, {'month': 'dec', 'total_sales': 0}]
+month = input('What month do you want to see total sales for? ')
+sales = 0
+with open('Project/sales.csv', 'r') as csv_file:    # Displays certain month's total sales
+    spreadsheet = csv.DictReader(csv_file)
+    for row in spreadsheet:
+        if month == row['month']:
+            sales += int(row['sales'])
+    print(f'The total sales for the month of {month} was ${sales}.00')
+
+with open('Project/sales.csv', 'r') as csv_file:    # Displays all month total sales
+    spreadsheet = csv.DictReader(csv_file)
+    for row in spreadsheet:
+        for i in range(0, len(months_dict)):
+            if row['month'] == months_dict[i]['month']:
+                months_dict[i]['total_sales'] += int(row['sales'])
+    print(months_dict)
