1768e8759508462f3b7241718f361083ed118d38
Wentana Ghile <137902774+wghile@users.noreply.github.com>
2023-12-10 11:42:40 -0500

    added some formatting to output

--- main.py
+++ main.py
@@ -7,29 +7,29 @@ with open('Project/sales.csv', 'r') as csv_file:
     spreadsheet = csv.DictReader(csv_file)
 
 # 2. Collect all of the sales from each month into a single list
+print(f'List of All Sales')
+print(f'----------------------------')
 with open('Project/sales.csv', 'r') as csv_file:
     spreadsheet = csv.DictReader(csv_file)
     list = []
     for row in spreadsheet:
         list.append(row)
-    # print(list)
+    print(list)
 
 # 3. Output the total sales across all months
 months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
 months_dict = [{'month': 'jan', 'total_sales': 0}, {'month': 'feb', 'total_sales': 0}, {'month': 'mar', 'total_sales': 0}, {'month': 'apr', 'total_sales': 0}, {'month': 'may', 'total_sales': 0}, {'month': 'jun', 'total_sales': 0}, {'month': 'jul', 'total_sales': 0}, {'month': 'aug', 'total_sales': 0}, {'month': 'sep', 'total_sales': 0}, {'month': 'oct', 'total_sales': 0}, {'month': 'nov', 'total_sales': 0}, {'month': 'dec', 'total_sales': 0}]
-month = input('What month do you want to see total sales for? ')
-sales = 0
-with open('Project/sales.csv', 'r') as csv_file:    # Displays certain month's total sales
-    spreadsheet = csv.DictReader(csv_file)
-    for row in spreadsheet:
-        if month == row['month']:
-            sales += int(row['sales'])
-    print(f'The total sales for the month of {month} was ${sales}.00')
-
-with open('Project/sales.csv', 'r') as csv_file:    # Displays all month total sales
+print(f'\n\nSummary of Total Sales per Month')
+print(f'----------------------------')
+with open('Project/sales.csv', 'r') as csv_file:    # Displays list of each month's total sales
     spreadsheet = csv.DictReader(csv_file)
     for row in spreadsheet:
         for i in range(0, len(months_dict)):
             if row['month'] == months_dict[i]['month']:
                 months_dict[i]['total_sales'] += int(row['sales'])
     print(months_dict)
+month = input('\nWhat month do you want to see total sales for? ') 
+sales = 0
+for object in months_dict:      # Displays certain month's total sales
+    if object['month'] == month:
+        print(f'The total sales for the month of {month} was ${object['total_sales']}.00\n')
